1b0657dd47b6362078a238d5f6f48ca5c178f7e4
mooooburg-dev <jeongmupark@gmail.com>
2022-01-06 20:13:22 +0900

    카운터 앱 Cypress E2E 테스트 코드

--- cypress/integration/couter.spec.js
+++ cypress/integration/couter.spec.js
@@ -1,9 +1,68 @@
+// - [ ] counter의 초기값은 0이다.
+// - [ ] + 버튼을 클릭 시 count가 1증가한다.
+// - [ ] - 버튼을 클릭 시 count가 1감소한다.
+// - [ ] + 버튼을 클릭 시 count가 10이 넘는 경우 더이상 증가하지 못한다. (Max 값이 10)
+// - [ ] - 버튼을 클릭 시 count가 0보다 작아지는 경우 감소하지 못한다. (Min 값이 0)
+// - [ ] reset 버튼을 클릭 시 counter가 0으로 초기화된다.
+
 describe("example counter-app", () => {
   beforeEach(() => {
     cy.visit("http://127.0.0.1:5500/index.html");
   });
 
   it("최초에 카운터 값을 0으로 보여준다", () => {
-    cy.get("#value").invoke("text").should("eq", "1");
+    cy.get("#value").invoke("text").should("eq", "0");
+  });
+
+  it("+ 버튼을 클릭 시 count가 1증가한다.", () => {
+    // 먼저 기존 값을 가져오고
+    // + 버튼을 클릭한 다음에
+    // 변화된 값이 기존값 + 1인지 체크
+    cy.get("#value")
+      .invoke("text")
+      .then((value) => {
+        const preValue = Number(value);
+        cy.get(".increase-btn").click();
+        cy.get("#value")
+          .invoke("text")
+          .should("eq", String(preValue + 1));
+      });
+  });
+
+  it("- 버튼을 클릭 시 count가 1감소한다", () => {
+    // (html에서 최소값이 0으로 설정되어 있어 -1로 내려가지 않기 때문에)
+    // + 버튼을 클릭해서 값을 1로 만든다
+    // 기존 값을 가져오고
+    // - 버튼을 클릭한 다음에
+    // 변화된 값이 기존값 - 1인지 체크
+    cy.get(".increase-btn").click();
+    cy.get("#value")
+      .invoke("text")
+      .then((value) => {
+        const preValue = Number(value);
+
+        cy.get(".decrease-btn").click();
+        cy.get("#value")
+          .invoke("text")
+          .should("eq", String(preValue - 1));
+      });
+  });
+
+  it("+ 버튼을 클릭 시 count가 10이 넘는 경우 더이상 증가하지 못한다. (Max 값이 10)", () => {
+    for (let i = 0; i < 13; i++) {
+      cy.get(".increase-btn").click();
+    }
+    cy.get("#value").invoke("text").should("eq", "10");
+  });
+
+  it("- 버튼을 클릭 시 count가 0보다 작아지는 경우 감소하지 못한다. (Min 값이 0)", () => {
+    cy.get(".decrease-btn").click();
+    cy.get("#value").invoke("text").should("eq", "0");
+  });
+
+  it("reset 버튼을 클릭 시 counter가 0으로 초기화된다.", () => {
+    cy.get(".increase-btn").click();
+    cy.get(".reset-btn").click();
+    cy.get("#value").invoke("text").should("eq", "0");
   });
 });
